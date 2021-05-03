5a656ee385d555bc9e837ae6db3dc89504110d2b
acachenaut <aitor.pilota@yahoo.fr>
2021-05-03 16:18:02 +0200

    V1.2

--- client.c
+++ client.c
@@ -12,33 +12,19 @@
 #define TAILLEBUF 20
 int sock;
 
-/*int creerSocket(int port, int type)
-{
-    static struct sockaddr_in adresse;
-    int sock;
+int creerSocket(int port, int type){
     sock = socket(AF_INET, type, 0);
     if (sock == -1) {
         return -1;
     }
-    bzero((char *) &adresse, sizeof(adresse));
-    adresse.sin_family = AF_INET;
-    adresse.sin_port = htons(port);
-    adresse.sin_addr.s_addr=htonl(INADDR_ANY);
-    if(bind(sock, (struct sockaddr*)&adresse, sizeof(adresse))== -1) {
-        return -1;
-    }
-        return sock;
-}
-
-int creerSocketUDP(int port){
-    return (creerSocket(port, SOCK_DGRAM));
+    return sock;
 }
 
 int creerSocketTCP(int port){
     return (creerSocket(port, SOCK_STREAM));
 }
 
-int connexion_serveur(char* argv[], int sock){
+int connexion_serveur(char* argv[]){
     static struct sockaddr_in addr_serveur;
     struct hostent *host_serveur;
     int port;
@@ -58,92 +44,43 @@ int connexion_serveur(char* argv[], int sock){
     return 0;
 }
 
-int main(int argc, char *argv[]){
-    int sock;
-    char *message = "bonjour";
-    char reponse[TAILLEBUF];
-    int port;
-    int nb_octets;
-
-    port = atoi(argv[2]);
-    if((sock = creerSocketTCP(port)==-1)){
-        perror("Erreur création socket");
-        return 1;
-    }
-    if (connexion_serveur(argv, sock) == -1){
-        perror("Erreur connexion socket serveur");
-        return 1;
-    }
-    printf(" connexion serveur etablie\n");
-
-    nb_octets = write(sock, message, strlen(message)+1);
-    nb_octets = read(sock, reponse, TAILLEBUF);
-    if(nb_octets==0){
-        perror("Rien reçu");
-        return 1;
-    }
-    printf(" reponse recue : %s\n", reponse);
-
-    close(sock);
-
-    return 0;
-}*/
-
 char* connexion_tcp(int pseudo){
     struct requete req;
     char *message;
+    char pseudoChar[20];
+    char* reponse = malloc(sizeof(char)* TAILLEBUF);
     int taille_msg;
-    char* resultat;
+    sprintf(pseudoChar, "%d", pseudo);
     req.type_requete = CONNEXION_TCP;
-    req.taille_requete = sizeof(int);
-    taille_msg = sizeof(struct requete) + sizeof(int);
+    req.taille_requete = sizeof(pseudoChar);
+    taille_msg = sizeof(struct requete) + 20;
     message = (char *) malloc(taille_msg);
     memcpy(message, &req, sizeof(struct requete));
-    memcpy(message+sizeof(struct requete), &pseudo, sizeof(int));
+    memcpy(message+sizeof(struct requete), &pseudoChar, 20);
     if (write(sock, message, taille_msg) <= 0){
         free(message);
         return NULL;
     }
-    if (read(sock, (char *)&resultat, sizeof(long)) <= 0){
+    if (read(sock, (char *)&reponse, sizeof(reponse)) <= 0){
         free(message);
         return NULL;
     }
     free(message);
-    return resultat;
+    return reponse;
 }
 
 int main(int argc, char* argv[]){
-    static struct sockaddr_in addr_serveur;
-    struct hostent *host_serveur;
     int port;
-    char pseudo[TAILLEBUF];
     char* reponse;
-    sprintf(pseudo,"%d" ,getpid());
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        perror("creation socket");
-        exit(1);
-    }
-    host_serveur = gethostbyname(argv[1]);
-    if (host_serveur==NULL)  {
-        perror("erreur récupération adresse serveur\n");
-        exit(1);
-    }
-    bzero((char *) &addr_serveur, sizeof(addr_serveur));
-    addr_serveur.sin_family = AF_INET;
     port = atoi(argv[2]);
-    addr_serveur.sin_port = htons(port);
-    memcpy(&addr_serveur.sin_addr.s_addr, host_serveur->h_addr, host_serveur->h_length);
-    if (connect(sock, (struct sockaddr *)&addr_serveur, sizeof(struct sockaddr_in)) == -1) {
-        perror("erreur connexion serveur");
-        exit(1);
+    if (creerSocketTCP(port) == -1){
+      perror("creation socket");
+      return 1;
     }
-    /*nb_octets = write(sock, pseudo, strlen(pseudo)+1);
-    nb_octets = read(sock, reponse, TAILLEBUF);
-    if(nb_octets==0){
-        perror("Rien reçu");
-        return 1;
-    }*/
+    if (connexion_serveur(argv) == -1){
+      perror("erreur connexion serveur");
+      return 1;
+    }
     reponse = connexion_tcp(getpid());
     if (reponse == NULL){
         perror("Connexion TCP");
--- serveur.c
+++ serveur.c
@@ -12,56 +12,6 @@
 
 #define TAILLEBUF 20
 
-/*int main(int argc, char* argv[]){
-    static struct sockaddr_in addr_client;
-    static struct sockaddr_in addr_serveur;
-    int lg_addr, opt, port;
-    int socket_ecoute, socket_service;
-    char message[TAILLEBUF];
-    char *chaine_recue;
-    char *reponse = "bien recu";
-    int nb_octets;
-    socket_ecoute = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_ecoute == -1) {
-        perror("creation socket");
-        exit(1);
-    }
-    bzero((char *) &addr_serveur, sizeof(addr_serveur));
-    opt = 1;
-    if (setsockopt(socket_ecoute, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    addr_serveur.sin_family = AF_INET;
-    port = atoi(argv[1]);
-    addr_serveur.sin_port = htons(port);
-    addr_serveur.sin_addr.s_addr=htonl(INADDR_ANY);
-    if( bind(socket_ecoute, (struct sockaddr*)&addr_serveur, sizeof(addr_serveur))== -1 ) {
-        perror("erreur bind socket écoute");
-        exit(1);
-    }
-    if (listen(socket_ecoute, 5) == -1) {
-        perror("erreur listen");
-        exit(1);
-    }
-    lg_addr = sizeof(struct sockaddr_in);
-    socket_service = accept(socket_ecoute, (struct sockaddr *) &addr_client, (socklen_t *) &lg_addr);
-    if (socket_service == -1) {
-        perror("erreur accept");
-        exit(1);
-    }
-
-    nb_octets = read(socket_service, message, TAILLEBUF);
-    chaine_recue =(char *)malloc(nb_octets * sizeof(char));
-    memcpy(chaine_recue, message, nb_octets);
-    printf("reçu pseudo %s\n", chaine_recue);
-    write(socket_service, reponse, strlen(reponse)+1);
-    close(socket_service);
-    close(socket_ecoute);
-
-    return 0;
-}*/
-
 int creerSocket(int port, int type){
     static struct sockaddr_in adresse;
     int sock;
@@ -76,16 +26,15 @@ int creerSocket(int port, int type){
     if(bind(sock, (struct sockaddr*)&adresse, sizeof(adresse))== -1) {
         return -1;
     }
-        return sock;
+    return sock;
 }
 
 int creerSocketTCP(int port){
     return (creerSocket(port, SOCK_STREAM));
 }
 
-char* connexion_tcp(int pseudo){
-    char* reponse = "bien reçu";
-    return reponse;
+char* connexion_tcp(){
+    return "bien reçu";
 }
 
 void gererClient(int sock_client){
@@ -96,20 +45,17 @@ void gererClient(int sock_client){
         nb_octets = read(sock_client, &req, sizeof(struct requete));
         if (nb_octets <= 0){
             perror(" reception donnees");
-            break;
         }
         if (req.type_requete==CONNEXION_TCP){
-            int pseudo;
+            char pseudo[TAILLEBUF];
             char* res;
-            nb_octets = read(sock_client, &pseudo, sizeof(int));
+            nb_octets = read(sock_client, &pseudo, sizeof(pseudo));
             if (nb_octets <= 0){
                 perror(" reception donnees");
-                break;
             }
-            res = connexion_tcp(pseudo);
+            res = connexion_tcp();
             if( write(sock_client, (char *)&res, sizeof(res)) <= 0){
                 perror(" envoi reponse\n");
-                break;
             }
         }
     }
